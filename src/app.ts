8164802469222ba54d08241873c34759b5f25efa
kaatreHx <helloworld00ff@gmail.com>
2026-09-03 00:56:20 +0545

    kyc path added

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
 import authRoutes from './modules/auth/auth.routes';
+import userRoutes from './modules/users/users.routes';
+import kycRoutes from './modules/kyc/kyc.routes';
 
 const app = express();
 
@@ -15,5 +18,8 @@ app.use(
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/kyc', kycRoutes);
+app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 
 export default app;
